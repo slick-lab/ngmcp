987b8a8bff4dfe24bae589f43b32a3b0e4891c88
ZenDrx <ynwghosted@icloud.com>
2026-05-09 04:00:01 +0100

    Refactor token validation response handling

--- backend/src/google.cr
+++ backend/src/google.cr
@@ -1,21 +1,16 @@
-require "http/client"
-require "db"
-
 def validate_token(token : String)
   response = HTTP::Client.get("https://oauth2.googleapis.com/tokeninfo?id_token=#{token}")
+  
   if response.status_code == 200
-   user_data = JSON.parse(response.body)
-   email = user_data["email"].as_s
-   name = user_data["name"]?.try &.as_s || email
-   success, error = Db.find_or_create_user(email, name)
-   if success == true 
-     user_id = Db.get_id(email)
-     {"success": "true", "status": "okay", "id": "#{user_id}"}
-   end 
- else 
-  {"success": "false", "message": "invalid token or possible network error"}
- end 
-end 
-  
-  
-  
+    user_data = JSON.parse(response.body)
+    email = user_data["email"].as_s
+    name = user_data["name"]?.try &.as_s || email
+    
+    Db.find_or_create_user(email, name)
+    user_id = Db.get_id(email)
+    
+    {"success": true, "user_id": user_id, "email": email}.to_json
+  else
+    {"success": false, "message": "Invalid Google token"}.to_json
+  end
+end
